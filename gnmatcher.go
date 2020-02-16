aad0e2786f740e379ab1219c190d090ec3fb26e3
Dmitry Mozzherin <dmozzherin@gmail.com>
2020-02-16 12:12:28 -0600

    config file and version opt

--- gnmatcher.go
+++ gnmatcher.go
@@ -1,4 +1,82 @@
 package gnmatcher
 
+// GNmatcher keeps most general configuration settings and high level
+// methods for scientific name matching.
 type GNmatcher struct {
+	WorkDir string
+	JobsNum int
+	PgHost  string
+	PgPort  int
+	PgUser  string
+	PgPass  string
+	PgDB    string
+}
+
+// NewGNmatcher is a constructor for GNmatcher instance
+func NewGNmatcher(opts ...Option) GNmatcher {
+	gnm := GNmatcher{
+		WorkDir: "/tmp/gnmatcher",
+		JobsNum: 4,
+		PgHost:  "0.0.0.0",
+		PgPort:  5432,
+		PgUser:  "postgres",
+		PgPass:  "",
+		PgDB:    "gnindex",
+	}
+	for _, opt := range opts {
+		opt(&gnm)
+	}
+	return gnm
+}
+
+// Option is a type of all options for GNmatcher.
+type Option func(gnm *GNmatcher)
+
+// OptWorkDir sets a directory for key-value stores and temporary files.
+func OptWorkDir(d string) Option {
+	return func(gnm *GNmatcher) {
+		gnm.WorkDir = d
+	}
+}
+
+// OptJobsNum sets number of concurrent jobs to run for parallel tasks.
+func OptJobsNum(i int) Option {
+	return func(gnm *GNmatcher) {
+		gnm.JobsNum = i
+	}
+}
+
+// OptPgHost sets the host of gnindex database
+func OptPgHost(h string) Option {
+	return func(gnm *GNmatcher) {
+		gnm.PgHost = h
+	}
+}
+
+// OptPgUser sets the user of gnindex database
+func OptPgUser(u string) Option {
+	return func(gnm *GNmatcher) {
+		gnm.PgUser = u
+	}
+}
+
+// OptPgPass sets the password to access gnindex database
+func OptPgPass(p string) Option {
+	return func(gnm *GNmatcher) {
+		gnm.PgPass = p
+	}
+}
+
+// OptPgPort sets the port for gnindex database
+func OptPgPort(p int) Option {
+	return func(gnm *GNmatcher) {
+		gnm.PgPort = p
+	}
+}
+
+// OptPgDB sets the name of gnindex database
+func OptPgDB(n string) Option {
+	return func(gnm *GNmatcher) {
+		gnm.PgDB = n
+	}
 }
--- gnmatcher/cmd/root.go
+++ gnmatcher/cmd/root.go
@@ -22,82 +22,137 @@ THE SOFTWARE.
 package cmd
 
 import (
-  "fmt"
-  "os"
-  "github.com/spf13/cobra"
+	"fmt"
+	"log"
+	"os"
 
-  homedir "github.com/mitchellh/go-homedir"
-  "github.com/spf13/viper"
+	"github.com/gnames/gnmatcher"
+	"github.com/spf13/cobra"
 
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
 )
 
+var (
+	cfgFile string
+	opts    []gnmatcher.Option
+)
 
-var cfgFile string
-
+// config purpose is to achieve automatic import of data from the
+// configuration file, if it exists.
+type config struct {
+	WorkDir string
+	PgHost  string
+	PgPort  int
+	PgUser  string
+	PgPass  string
+	PgDB    string
+	JobsNum int
+}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-  Use:   "gnmatcher",
-  Short: "A brief description of your application",
-  Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Use:   "gnmatcher",
+	Short: "Contains tools and algorithms to verify scientific names",
+	Run: func(cmd *cobra.Command, args []string) {
+		version, err := cmd.Flags().GetBool("version")
+		if err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
+		if version {
+			fmt.Printf("\nversion: %s\nbuild: %s\n\n", gnmatcher.Version, gnmatcher.Build)
+			os.Exit(0)
+		}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-  // Uncomment the following line if your bare application
-  // has an action associated with it:
-  //	Run: func(cmd *cobra.Command, args []string) { },
+		if len(args) == 0 {
+			_ = cmd.Help()
+			os.Exit(0)
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-  if err := rootCmd.Execute(); err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
 func init() {
-  cobra.OnInitialize(initConfig)
+	cobra.OnInitialize(initConfig)
 
-  // Here you will define your flags and configuration settings.
-  // Cobra supports persistent flags, which, if defined here,
-  // will be global for your application.
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
 
-  rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gnmatcher.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gnmatcher.yaml)")
 
-
-  // Cobra also supports local flags, which will only run
-  // when this action is called directly.
-  rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("version", "V", false, "Return version")
 }
-
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-  if cfgFile != "" {
-    // Use config file from the flag.
-    viper.SetConfigFile(cfgFile)
-  } else {
-    // Find home directory.
-    home, err := homedir.Dir()
-    if err != nil {
-      fmt.Println(err)
-      os.Exit(1)
-    }
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 
-    // Search config in home directory with name ".gnmatcher" (without extension).
-    viper.AddConfigPath(home)
-    viper.SetConfigName(".gnmatcher")
-  }
+		// Search config in home directory with name ".gnmatcher" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".gnmatcher")
+	}
 
-  viper.AutomaticEnv() // read in environment variables that match
+	viper.AutomaticEnv() // read in environment variables that match
 
-  // If a config file is found, read it in.
-  if err := viper.ReadInConfig(); err == nil {
-    fmt.Println("Using config file:", viper.ConfigFileUsed())
-  }
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		log.Println("Using config file:", viper.ConfigFileUsed())
+	} else {
+		fmt.Println("Config file $HOME/.gnidump.yaml not found")
+	}
+	getOpts()
 }
 
+// getOpts imports data from the configuration file. Some of the settings can
+// be overriden by command line flags.
+func getOpts() []gnmatcher.Option {
+	cfg := &config{}
+	err := viper.Unmarshal(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if cfg.WorkDir != "" {
+		opts = append(opts, gnmatcher.OptWorkDir(cfg.WorkDir))
+	}
+	if cfg.JobsNum != 0 {
+		opts = append(opts, gnmatcher.OptJobsNum(cfg.JobsNum))
+	}
+	if cfg.PgHost != "" {
+		opts = append(opts, gnmatcher.OptPgHost(cfg.PgHost))
+	}
+	if cfg.PgPort != 0 {
+		opts = append(opts, gnmatcher.OptPgPort(cfg.PgPort))
+	}
+	if cfg.PgUser != "" {
+		opts = append(opts, gnmatcher.OptPgUser(cfg.PgUser))
+	}
+	if cfg.PgPass != "" {
+		opts = append(opts, gnmatcher.OptPgPass(cfg.PgPass))
+	}
+	if cfg.PgDB != "" {
+		opts = append(opts, gnmatcher.OptPgDB(cfg.PgDB))
+	}
+	return opts
+}
